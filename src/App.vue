da3a3a1503e1af14d75c1d4c85e20f05ae094cd3
P-Thanida <s6135512015@phuket.psu.ac.th>
2022-05-31 15:06:28 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Call API</h1>
+  <p v-for="item in list" :key="item.id">
+  {{item.id}} {{item.email}}---{{item.first_name}} {{item.last_name}}
+  </p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+const baseURL = "https://reqres.in/api/users?page=1";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      list: []
+    }
+  },
+  async mounted() 
+    {
+      let result = await axios.get(baseURL);
+      console.warn(result.data.data)
+      this.list = result.data.data
+    } 
+};
 </script>
 
 <style>
@@ -22,5 +33,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text-align: left;
+}
+
+ul {
+  margin: 0px;
+  padding: 0px;
+}
+
+li {
+  list-style: none;
 }
 </style>
